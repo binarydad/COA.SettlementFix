20163cb1e277064dbfe3be163f3a5cf64548bf4a
Ryan Peters <rpeters@it.clearoneadvantage.com>
2020-09-18 19:10:40 -0400

    formatting

--- Creditor.cs
+++ Creditor.cs
@@ -4,17 +4,17 @@ namespace COA.SettlementFix
 {
     public class Creditor
     {
-		public int CreditorId { get; set; }
-		public int SettlementAttemptId { get; set; }
-		public DateTime ResponseDate { get; set; }
-		public int RejectReasonId { get; set; }
-		public string RejectReasonComment { get; set; }
-		public string AttemptedStatus { get; set; }
-		public DateTime AttemptCreated { get; set; }
-		public string CurrentStatus { get; set; }
-		public string ClientName { get; set; }
-		public string AccountNumber { get; set; }
-		public decimal CurrentBalance { get; set; }
-		public string CreditorName { get; set; }
+        public int CreditorId { get; set; }
+        public int SettlementAttemptId { get; set; }
+        public DateTime ResponseDate { get; set; }
+        public int RejectReasonId { get; set; }
+        public string RejectReasonComment { get; set; }
+        public string AttemptedStatus { get; set; }
+        public DateTime AttemptCreated { get; set; }
+        public string CurrentStatus { get; set; }
+        public string ClientName { get; set; }
+        public string AccountNumber { get; set; }
+        public decimal CurrentBalance { get; set; }
+        public string CreditorName { get; set; }
     }
 }
